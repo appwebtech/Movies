f1be5bab53e1e36db34e326b31e2dfc639eafcab
Joseph <josembi@gmail.com>
2017-11-25 23:03:14 +0100

    Movies Functionality on Index Action (cRud)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,12 @@
 class MoviesController < ApplicationController
 #	before_action :josembi_koding_late (:index, :new)
+	before_action :feeling_blessed, only: [:show, :edit, :update, :destroy]
 
 	def index
-		
+		@movies = Movie.all.order("created_at DESC")
+	end
+
+	def show
 	end
 
 	def new
@@ -11,6 +15,13 @@ class MoviesController < ApplicationController
 
 	def create
 		@movie = Movie.new(josembi_koding_late)
+		if @movie.save
+			flash[:success] = "Movie Created successfully"
+			redirect_to root_path
+		else
+			flash[:primary] = "Something went wrong...."
+			render_new
+		end
 	end
 
 	def update
@@ -25,6 +36,10 @@ class MoviesController < ApplicationController
 
 	def josembi_koding_late
 		params.require(:movie).permit(:title, :description, :director)
+	end
+
+	def feeling_blessed
+		@movie = Movie.find(params[:id])
 	end
 
 end
