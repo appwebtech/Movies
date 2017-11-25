27fe518b6f25999a313c08109d7f922a153633fe
Joseph <josembi@gmail.com>
2017-11-25 22:22:25 +0100

    New & Create Actions (Crud)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,30 @@
 class MoviesController < ApplicationController
+#	before_action :josembi_koding_late (:index, :new)
 
 	def index
 		
 	end
-	
+
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		@movie = Movie.new(josembi_koding_late)
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def josembi_koding_late
+		params.require(:movie).permit(:title, :description, :director)
+	end
+
 end
