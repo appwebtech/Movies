64d1df4682598feeed12eea51918d747dedd4dcf
Joseph <josembi@gmail.com>
2017-11-27 22:41:07 +0100

    Routes and Methods for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,29 @@
 class ReviewsController < ApplicationController
+	before_action :kamuthenya_kafoca
 
 	def new
-		
+		@review = Review.new
 	end
 
 	def create
-		
+		@review = Review.new(kamuthenya)
+		@review.movie_id = @movie_id
+		@review.user_id = current_user.id
+
+		if @review.save
+			redirect_to movie_path(@movie)
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def kamuthenya
+		params.require(:review).permit(:rating, :comment)
+	end
+
+	def kamuthenya_kafoca
+		@movie = Movie.find(params[:movie_id])
 	end
 end
